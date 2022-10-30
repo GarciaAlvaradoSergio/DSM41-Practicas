50be3699c9cf637927eda13bdfc547674996fdd6
Sergio Garcia <al222110820@gmail.com>
2022-10-30 11:33:38 -0600

    migrations

---
+++ database/migrations/2022_10_30_171606_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->mediumText('discription');
+            $table->mediumText('content');
+            $table->timestamp('date')->nullable();
+            $table->unsignedInteger('category_id'); //Relacion con categoria.
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
