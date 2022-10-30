2c401acb497e53696b9d30cf7bd755551ffdad10
Sergio Garcia <al222110820@gmail.com>
2022-10-30 12:38:13 -0600

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $this->call(UserSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(BookSeeder::class);
+        $this->call(TagSeeder::class);
     }
 }
