5bf457657c3e0f91574b2f7d28d9696ecac2d12b
Sergio Garcia <al222110820@gmail.com>
2022-11-03 23:29:48 -0600

    note

--- resources/views/layouts/container.blade.php
+++ resources/views/layouts/container.blade.php
@@ -4,90 +4,22 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Información</h1>
-        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                class="fas fa-download fa-sm text-white-50"></i> Generar reporte</a>
+<div class="row" style="justify-content: center">
+<div class="card text-center">
+  <div class="card-body">
+<form class="row gy-2 gx-3 align-items-center">
+<div class="col-auto" style="justify-content:center">
+    <div class="input-group" >
+      <input style="width: 20rem;" type="text" class="form-control" id="autoSizingInputGroup" placeholder="Titulo">
+      <a href=""><div class="input-group-text"><i class="fa-solid fa-image" style="margin: 4px;"></i></div></a>
     </div>
+  </div>
+  </div>
+</div>
+</form>
 
 
-
-    <!-- Content Row -->
-
-    <div class="row">
-
-        <!-- Area Chart -->
-        <div class="col-xl-8 col-lg-7">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Plantilla de tabla</h6>
-                    <div class="dropdown no-arrow">
-                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                        </a>
-                        <!-- Dropdown - User Information -->
-                        <div class="dropdown-menu shadow animated--grow-in" aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Agregar
-                            </a>
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Editar
-                            </a>
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Ver
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Eliminar
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Libro</th>
-                                    <th>Categoria</th>
-                                    <th>Estudiante</th>
-                                </tr>
-                            </thead>
-                            <tfoot>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Libro</th>
-                                    <th>Categoria</th>
-                                    <th>Estudiante</th>
-                                </tr>
-                            </tfoot>
-                            <tbody>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-    <!-- Content Row -->
-    <div class="row">
-
-
-        <div class="col-lg-6 mb-4">
-        </div>
-    </div>
-
+</div>
 </div>
 <!-- /.container-fluid -->
 
