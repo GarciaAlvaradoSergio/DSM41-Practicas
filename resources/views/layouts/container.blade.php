06292d7c1ecf64952335f2a0189081f1680bc4e1
Sergio Garcia <al222110820@gmail.com>
2022-11-06 15:09:16 -0600

    delete

--- resources/views/layouts/container.blade.php
+++ resources/views/layouts/container.blade.php
@@ -3,15 +3,6 @@
 
 <!-- Begin Page Content -->
 <div class="container-fluid">
-  <form action="" method="post">
-  <div class="search-box">
-    <input type="text" placeholder="Titulo..." class="search-input"  name=""/>
-    <input type="text" placeholder="Crear una nota..." class="search-input-note" name=""autofocus/>
-    <a href="#" class="search-btn">
-      <i class="fa-solid fa-pen-to-square"></i>
-    </a>
-    </form>
-  </div>
 
 
 </div>
